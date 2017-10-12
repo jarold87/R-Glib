2b859d4427720bd66d06c0127227084dcd754df9
Viktor Simkó <viktor.simko@innonic.com>
2017-10-12 17:08:51 +0200

    start optimization

--- R/dataTransformation.R
+++ R/dataTransformation.R
@@ -18,13 +18,14 @@ GlibDataTransformation <- function(GlibEnvironment) {
     read = function(file, abs = FALSE) {
       filePath <- GlibEnvironment$getFilePath('inputDir', file, abs)
       data <- tryCatch( {
-        read.table(filePath, quote='"', header=TRUE, sep=",", fill = TRUE)
+        fread(filePath, quote='"', header=TRUE, sep=",", fill = TRUE)
       }, warning = function(war) {
         return(FALSE)
       }, error = function(err) {
         return(FALSE)
       })
       data <- prepare(data)
+
       assign("trData", data, thisEnv)
       remove(data)
     },
@@ -37,8 +38,8 @@ GlibDataTransformation <- function(GlibEnvironment) {
       if (!unit %in% names(df)) {
         stop(paste("Glib: ", "Unit is not exist!", " (", unit, ")", sep=""))
       }
-      d[,paste(unit, 'GlibTemp', sep="_")] <- as.integer(format(as.POSIXct(d[,dc]), format=df[unit]))
-      data <- data[d[,paste(unit, 'GlibTemp', sep="_")] %in% values,]
+      d[(paste(unit, 'GlibTemp', sep="_")) := as.integer(format(as.POSIXct(d[[dc]]), format=df[unit]))]
+      data <- data[d[[paste(unit, 'GlibTemp', sep="_")]] %in% values,]
       assign("trData", data, thisEnv)
     },
 
@@ -47,25 +48,25 @@ GlibDataTransformation <- function(GlibEnvironment) {
       df <- getConfig('dateUnitAndFormat')
       dc <-getConfig('dateColumn')
       v <- lapply(df, function(x) {
-        as.integer(format(as.POSIXct(data[,dc]), format=x))
+        as.integer(format(as.POSIXct(data[[dc]]), format=x))
       })
       m <- matrix(unlist(v), nrow = nrow(data), byrow = FALSE)
-      data[,attributes(df)$names] <- m
+      data[,(attributes(df)$names) := m]
       assign("trData", data, thisEnv)
     },
 
     normalizeColumnValues = function(column, oldValues = c(), newValue) {
       data <- get("trData", thisEnv)
-      values <- data[,column]
+      values <- data[[column]]
       if (!is.factor(values)) {
-        data[,column] <- values[values %in% oldValues] <- newValue
+        data[,(column) := (values[values %in% oldValues] <- newValue)]
         return(data)
       }
       levels <- c(attributes(values)$levels, newValue)
       values <- as.vector(values)
       values[values %in% oldValues] <- newValue
       levels <- levels[!levels %in% oldValues]
-      data[,column] <- factor(values, levels = levels)
+      data[,(column) := factor(values, levels = levels)]
       assign("trData", data, thisEnv)
     },
 
@@ -79,19 +80,19 @@ GlibDataTransformation <- function(GlibEnvironment) {
         stop(paste("Glib: ", "Unit is not exist!", " (", unit, ")", sep=""))
       }
       tc <- 'GlibTemp_timestamp'
-      d[,tc] <- as.numeric(as.POSIXct(d[,dc]))
-      t <- aggregate(d[,tc], list(d[,uc]), function(x) x[length(x)] - x[1] )
+      d[,(tc) := as.numeric(as.POSIXct(d[[dc]]))]
+      t <- aggregate(d[[tc]], d[,uc,with=F], function(x) x[length(x)] - x[1] )
       filteredUsers <- as.vector(t[t[,2] <= value * ds[unit],1])
       if (filterUser) {
-        d <- d[d[,uc] %in% filteredUsers,]
+        d <- d[d[[uc]] %in% filteredUsers,]
         assign("trData", d, thisEnv)
         return()
       }
-      if (!nrow(d[!(d[,uc] %in% filteredUsers),])) {
+      if (!nrow(d[!(d[[uc]] %in% filteredUsers),])) {
         assign("trData", d, thisEnv)
         return()
       }
-      ci <- aggregate(d[!(d[,uc] %in% filteredUsers),tc], list(d[!(d[,uc] %in% filteredUsers),uc]), function(x) {
+      ci <- aggregate(d[!(d[[uc]] %in% filteredUsers),tc,with=F], d[!(d[[uc]] %in% filteredUsers),uc,with=F], function(x) {
         f <- x[1]
         r <- unlist(lapply(c(2:length(x)), function(i) {
           if (is.na(x[i])) return(c())
@@ -101,6 +102,7 @@ GlibDataTransformation <- function(GlibEnvironment) {
         return(0)
       })
       d <- cutByTimestamp(d, ci)
+      print('foo')
       assign("trData", d, thisEnv)
     },
 
@@ -110,31 +112,31 @@ GlibDataTransformation <- function(GlibEnvironment) {
       goalEvent <- getConfig('goalEvent')
       dc <- getConfig('dateColumn')
       uc <- getConfig('userIdColumn')
-      d[,tc] <- as.numeric(as.POSIXct(d[,dc]))
+      d[[,tc]] <- as.numeric(as.POSIXct(d[[,dc]]))
       goalReachedUsers <- getGoalReachedUsers(d)
       if (!length(goalReachedUsers)) return()
       goalEventTime <- lapply(goalReachedUsers, function(user) {
-        return(d[d$user_id == user & d$event == goalEvent, 'GlibTemp_timestamp'][1])
+        return(d[[d$user_id == user & d$event == goalEvent, 'GlibTemp_timestamp']][1])
       })
-      ci <- data.frame(user = goalReachedUsers, goalEventTime = unlist(goalEventTime))
+      ci <- data.table(user = goalReachedUsers, goalEventTime = unlist(goalEventTime))
       d <- cutByTimestamp(d, ci)
       assign("trData", d, thisEnv)
     },
-    
+
     dropEventLogsAfterAnEvent = function(event) {
       d <- get("trData", thisEnv)
       tc <- 'GlibTemp_timestamp'
       dc <- getConfig('dateColumn')
       uc <- getConfig('userIdColumn')
-      d[,tc] <- as.numeric(as.POSIXct(d[,dc]))
-      users <- unique(d[,uc])
+      d[[,tc]] <- as.numeric(as.POSIXct(d[[,dc]]))
+      users <- unique(d[[,uc]])
       goalReachedUsers <- getGoalReachedUsers(d)
       if (length(goalReachedUsers)) users <- users[!(users %in% goalReachedUsers)]
       if (!length(users)) return()
       goalEventTime <- lapply(users, function(user) {
         return(d[d$user_id == user & d$event == event, 'GlibTemp_timestamp'][1])
       })
-      ci <- data.frame(user = users, goalEventTime = unlist(goalEventTime))
+      ci <- data.table(user = users, goalEventTime = unlist(goalEventTime))
       d <- cutByTimestamp(d, ci)
       assign("trData", d, thisEnv)
     },
@@ -150,14 +152,14 @@ GlibDataTransformation <- function(GlibEnvironment) {
     dc <- getConfig('dateColumn')
     uc <- getConfig('userIdColumn')
     pc <- getConfig('productIdColumn')
-    data <- data[which(data[,dc] != ''),]
-    data[,uc] <- as.vector(as.character(data[,uc]))
-    data[,pc] <- as.vector(as.character(data[,pc]))
-    if (grepl('"', data[1,dc])) {
-      data[,dc] <- gsub('"', "", data[,dc])
+    data <- data[which(data[[dc]] != '')]
+    data[,(uc) := as.vector(as.character(data[[uc]]))]
+    data[,(pc) := as.vector(as.character(data[[pc]]))]
+    if (grepl('"', data[[1,dc]])) {
+      data[,(dc) := gsub('"', "", data[[dc]])]
     }
-    if (grepl('T', data[1,dc]) & grepl('Z', data[1,dc])) {
-      data[,dc] <- as.character(as.POSIXlt(as.POSIXct(data[,dc], "%Y-%m-%dT%H:%M:%S", tz="UTC"), getConfig('defaultTimeZone')))
+    if (grepl('T', data[[1,dc]]) & grepl('Z', data[[1,dc]])) {
+      data[,(dc) := as.character(as.POSIXlt(as.POSIXct(data[[dc]], "%Y-%m-%dT%H:%M:%S", tz="UTC"), getConfig('defaultTimeZone')))]
     }
     return(data)
   }
@@ -172,10 +174,13 @@ GlibDataTransformation <- function(GlibEnvironment) {
       unlist(lapply(c(1:nrow(cutTable)), function(i) {
         userId <- cutTable[i,1]
         cutTime <- cutTable[i,2]
-        if (cutTime > 0) rownames(d[d$user_id == userId & d[,tc] > cutTime, ])
+        if (cutTime > 0) {
+          rownames(d[d$user_id == userId & d[[tc]] > cutTime, ])
+        }
       }))
     }, mc.cores = length(groups))
-    d[unlist(ret),'GlibTemp_keep'] <- FALSE
+    print(typeof(unlist(ret)))
+    d[unlist(ret), GlibTemp_keep := FALSE]
     d <- d[d$GlibTemp_keep == TRUE,]
     d$GlibTemp_keep <- NULL
     return(d)
@@ -197,7 +202,7 @@ GlibDataTransformation <- function(GlibEnvironment) {
   getGoalReachedUsers <- function(data) {
     goalEvent <- getConfig('goalEvent')
     if (is.null(goalEvent) == FALSE) {
-      return(unique(data[data[,getConfig('eventColumn')] == goalEvent,getConfig('userIdColumn')]))
+      return(unique(data[data[[,getConfig('eventColumn')]] == goalEvent,getConfig('userIdColumn')]))
     }
     stop(paste("Glib UserProfiles: ", "Conversion event is missing!", sep=""))
   }
